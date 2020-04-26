55fb132d2029f43a121ff5a8de266ff5affe22be
MohamedGaber-intake40 <gaberm514@gmail.com>
2020-04-26 15:42:33 +0200

    remove previous Auth

--- blogApp/app/controllers/application_controller.rb
+++ blogApp/app/controllers/application_controller.rb
@@ -1,23 +1,5 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user, :only => [:home, :profile, :setting]
     before_action :save_login_state, :only => [:login, :login_attempt]
-    protected 
-    def authenticate_user
-        if session[:user_id]
-            # set current user object to @current_user object variable
-            @current_user = User.find session[:user_id] 
-            return true	
-        else
-            redirect_to(:controller => 'sessions', :action => 'login')
-            return false
-        end
-    end
-    def save_login_state
-        if session[:user_id]
-            redirect_to(:controller => 'sessions', :action => 'home')
-            return false
-        else
-            return true
-        end
-    end
+   
 end
